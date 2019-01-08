e86a385f93601b1a7ebea2f58fd78a3aec7b1ed0
Justen Quirante <jcquirante@gmail.com>
2019-01-08 11:57:18 -0800

    Initial project setup complete

---
+++ src/reducers/list_reducer.js
@@ -0,0 +1,16 @@
+import '../actions/types';
+
+const DEFAULT_STATE = {
+    all: [],
+    single: {}
+};
+
+
+
+export default (state=DEFAULT_STATE, action) => {
+    switch(action.type) {
+        
+        default:
+            return state;
+    }
+}
